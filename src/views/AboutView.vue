55a64b89c3748926af9bb07d35a23e4a7fad7411
karthikaditya <karthikaditya147@gmail.com>
2023-07-24 14:25:26 +0530

    nice page content added

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <iframe src='https://widgets.sociablekit.com/youtube-channel-videos/iframe/159193' frameborder='0' width='100%'
+    height='1000'></iframe> -->
+
+  
+</template>
+
+<style>
+/* @media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+</style>
---
+++ src/views/CalendarView.vue
@@ -0,0 +1,5 @@
+<template>
+    <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FKolkata&title=Calendar%20of%20events&showTz=1&showTabs=0&showPrint=0&showCalendars=0&showNav=1&src=a2FydGhpa2FkaXR5YTE0N0BnbWFpbC5jb20&color=%234285F4"
+        style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</template>
